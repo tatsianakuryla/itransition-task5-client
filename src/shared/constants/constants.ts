697a4db483c540a36fe1516c4a7cd38c046d10c0
Tatsiana Kuryla <tatianakuryla@gmail.com>
2025-10-08 16:05:00 +0200

    feat: update API_BASE_URL

--- src/shared/constants/constants.ts
+++ src/shared/constants/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:3000";
 
 export const DEFAULT_LOGIN_FORM_VALUES = {
   email: "",
